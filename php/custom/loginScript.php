1c87db0a6980588df82a11e20cb34704872d78e8
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-08-01 10:49:53 +0100

    functioning login and signup scripts, added shopping_cart.php file.

--- php/custom/loginScript.php
+++ php/custom/loginScript.php
@@ -7,10 +7,52 @@ $password = $_POST['password'];
 // Including connection script.
 include 'included_pages/db_connection.php';
 
+// starting sessions.
 
-// SQL CODE.
-$sql = "";
+if (session_start() && ($_SESSION['user_id'] != null)){
+  $user_id = $_SESSION['user_id'];
+  $sql = "SELECT cust_email, cust_password FROM `afriprints`.`customer` WHERE cust_id='$user_id'";
 
-// Compare user credentials to those stored in the database.
+  $stmt = $pdo->query($sql);
+  $stmt->setFetchMode(PDO::FETCH_NUM);
 
+  while ($row = $stmt->fetch()) {
+  $db_email = $row[0];
+  $db_password = $row[1];
+  }
+
+  if (($email_address == $db_email) && ($password == $db_password)) {
+  // Correct grant user acces to site.
+  echo "<br>user authentication, successful.";
+
+  header('Location: ../../html/index.php');
+  } else {
+  // Redirect users, login page and display appropriate error.
+  echo "<br>user authentication, failed.";
+
+  header('Location: ../../html/login.php');
+  // TODO: Attach error message to the header above.
+  }
+}else{
+  $sql =  "SELECT cust_email, cust_password FROM `afriprints`.`customer`";
+  $stmt = $pdo->query($sql);
+  $stmt->setFetchMode(PDO::FETCH_NUM);
+
+  while ($row = $stmt->fetch()) {
+  if(($email_address == $row[0]) && ($password == $row[0])){
+    // Correct grant user acces to site.
+    echo "<br>user authentication, successful.";
+
+    header('Location: ../../html/index.php');
+
+  } else {
+  // Redirect users, login page and display appropriate error.
+  echo "<br>user authentication, failed.";
+
+  header('Location: ../../html/login.php');
+  // TODO: Attach error message to the header above.
+  }
+}
+
+$pdo = null;
 ?>
--- php/custom/signupScript.php
+++ php/custom/signupScript.php
@@ -10,44 +10,49 @@ $confirm_password = $_POST['confirm_password'];
 
 $table_name = "customer";
 
-
 // Checking passwords match.
 if ($password == $confirm_password) {
-  // code...
   // Check the rest of user credentials.
   // Insert them into the database.
   // Including connection script.
   include 'included_pages/db_connection.php';
 
   // SQL CODE.
-  // $sql = "INSERT INTO `afriprints`.`customer` VALUES ('5', '$name', '$email_address', '$password', '', '$phone_number')";
-  // $pdo->exec($sql);
-
+  $sql = "INSERT INTO `afriprints`.`customer` VALUES ('6', '$name', '$email_address', '$password', '', '$phone_number')";
+  $pdo->exec($sql);
 
   // Storing customer's id in the sessions.
   session_start();
   $sql2 = "SELECT `cust_id` FROM `afriprints`.`customer` WHERE `cust_email`='$email_address'";
-  $result = $pdo->query($sql2);
+  $stmt = $pdo->query($sql2);
+  $stmt->setFetchMode(PDO::FETCH_NUM);
 
-  if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-          echo $row['cust_id'];
-      }
+  $result = $stmt->fetch();
+
+  foreach ($result as $row) {
+    echo "<br>Row number is: " . $row;
+    // Storing in sessions.
+    $_SESSION['user_id'] = $row;
+  }
+
+  // Saving user data in Sessions for the purpose of user validation.
+  $_SESSION['user_name'] = $name;
+  $_SESSION['email_address'] = $email_address;
+  $_SESSION['phone_number'] = $phone_number;
+  $_SESSION['password'] = $password;
+
+
+  // redirecting user to homepage.
+  header('Location: ../../html/index.php');
+
   }else {
     echo "nothing to display.";
     // Change and place appropriate use case there.
-  }
 
-
-  // redirect user upon successful login.
-
-
-
-} else {
-  // code...
-  // Present user with error message redirect him back signup page.
+    // redirecting user to signupage with appropriate error message.
+    header('Location: ../../html/signup.php');
 }
 
 
+$pdo = null;
 ?>
