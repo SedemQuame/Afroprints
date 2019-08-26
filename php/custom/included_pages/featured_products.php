674be88826dced705d0ed802fbc874c86332abc0
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-08-26 18:38:12 +0100

    changed featured_products.php.

--- php/custom/included_pages/featured_products.php
+++ php/custom/included_pages/featured_products.php
@@ -16,40 +16,52 @@
       </li>
     </ul>
   </div>
+
   <div class="album py-5">
-    <div class="container">
+    <!-- <div class="container">
       <h1 class="dress-section">Section Title.</h1>
-      <div class="row">
+      <div class="row"> -->
       <?php
+        $category = ['men', 'women', 'kids'];
         include_once 'db_connection.php';
-        $sql = "SELECT * FROM brand  WHERE brand_type ='".$page_name."';";
-        // $stmt = $pdo->query($sql);
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-        $stmt = $stmt->fetchAll();
+        $i = 0;
 
-        // Instantiating variable to hold populated pages.
-        $element = "";
-        foreach ($stmt as $row) {
-          $element .= '<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-                          <div class="card mb-4 shadow-sm">
-                            <img class="bd-placeholder-img card-img-top" src="'.$row['brand_image'].'" alt="" ';
+        for ($i=0; $i < sizeof($category); $i++) {
+          $sql = "SELECT * FROM brand  WHERE brand_type ='".$page_name."' AND brand_category = '".$category[$i]."';";
+          // $stmt = $pdo->query($sql);
+          $stmt = $pdo->prepare($sql);
+          $stmt->execute();
+          $stmt = $stmt->fetchAll();
 
-          $element .= '" alt="" width="100%" height="225">
-                      <div class="card-body">
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                          <form class="btn-group" action="../php/custom/cart-processor.php?action=add&item_id='.$row['brand_id'].'" method="post">
-                            <button onclick="openModal()" type="button" class="openBtn btn btn-sm btn-outline-secondary" data-number="'.$row['brand_id'].'">View Item</button>
-                            <button onclick="addToCart(this)" type="submit" class="btn btn-sm btn-outline-secondary" data-product-id="'.$row['brand_id'].'">Add To Cart</button>
-                          </form>
-                          <small class="text-muted">Ghc '.$row['brand_price'].'</small>
-                        </div>
-                      </div>
-                    </div>
-                  </div>';
+          if (!empty($stmt)) {
+            // Instantiating variable to hold populated pages.
+            $element = "";
+            $element .= '<div class="container mt-3">
+                            <h1 class="dress-section text-capitalize">'.$category[$i].'\'s Collection</h1>
+                            <hr />
+                            <div class="row">';
+            foreach ($stmt as $row) {
+
+              $element .= '<div class="col-sm-12 col-md-6 col-lg-4">
+                              <div class="card mb-4 shadow-sm">
+                                <img class="bd-placeholder-img card-img-top" src="'.$row['brand_image'].'" alt="" ';
+
+              $element .= '" alt="" width="100%" height="225">
+                          <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                              <form class="btn-group" action="../php/custom/cart-processor.php?action=add&item_id='.$row['brand_id'].'" method="post">
+                                <button onclick="openModal()" type="button" class="openBtn btn btn-sm btn-outline-secondary" data-number="'.$row['brand_id'].'">View Item</button>
+                                <button onclick="addToCart(this)" type="submit" class="btn btn-sm btn-outline-secondary" data-product-id="'.$row['brand_id'].'">Add To Cart</button>
+                              </form>
+                              <small class="text-muted">Ghc '.$row['brand_price'].'</small>
+                            </div>
+                          </div></div></div>';
+            }
+            $element .= '</div></div>';
+
+            echo $element;
+          }
         }
-
-        echo $element;
       ?>
 </main>
