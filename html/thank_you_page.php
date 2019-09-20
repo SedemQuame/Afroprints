f070aa0dfcd89e7cefaadfeb5d45240eb7d97524
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-09-20 20:13:27 +0100

    work on adding sharing buttons, to thank_you_page.css

--- html/thank_you_page.php
+++ html/thank_you_page.php
@@ -24,13 +24,25 @@
         // TODO: CREATE A CUSTOM LAYOUT FOR THE INDEX.PHP PAGE.
         // include '../php/custom/included_pages/featured_products.php';
     ?>
+
+        <!-- Go to www.addthis.com/dashboard to customize your tools -->
     <div class="glitch-container">
       <div id="thank_you_text" class="glitch" data-text="Thank You.">Thank You.</div>
     </div>
-    <p id="call_to_action" class="text-center">
-      Share What You Bought <br>
-      On <br>
-      Social Media.
-    </p>
+    <br>
+    <p id="call_to_action" class="text-center">Share What You Bought.</p>
+    <br>
+    <div class="addthis_inline_share_toolbox mx-auto"></div>
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d851ed86afd3dce"></script>
+    <script type="text/javascript">
+    // TODO: Change this and make it a little bit more dynamic.
+      var addthis_share = {
+         url: "C:\\xampp\\htdocs\\AfricanPrintSite\\html\\index.php",
+         title: "Sedem here.",
+         description: "THE DESCRIPTION",
+         media: "C:\\xampp\\htdocs\\AfricanPrintSite\\africanfashion\\tops\\top (5).jpg"
+      }
+    </script>
   </body>
 </html>
