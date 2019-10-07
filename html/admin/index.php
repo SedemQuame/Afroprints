62cc0c41003aa0603c25d59bfc455cb0214fe776
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-10-07 09:53:38 +0100

    working version, of products pane for admins to add different products, into the database.

--- html/admin/index.php
+++ html/admin/index.php
@@ -20,8 +20,128 @@
     <div class="col-10 col-md-9 dashboard">
       <!-- Place various, tabs here. -->
       <p>Index Page &lt; Page for adding new products. &gt;</p>
+      <!--
+      Things to include.
+      * Brand name
+      * Brand description
+      * Brand images
+      * Item type
+      * Item Price
+      * Item category
+      -->
+
+      <div class="">
+        <img src="holder.js/300x200">
+      </div>
+
+      <form action="../../php/custom/upload_items.php" method="post" enctype="multipart/form-data">
+        <div class="">
+          <label for="">Item Name </label>
+          <input class="form-control" type="text" id="" name="item_name" required>
+        </div>
+
+        <div class="">
+          <label for="">Item Price </label>
+          <input class="form-control" type="text" id="" name="item_price" required>
+        </div>
+
+        <div class="">
+          <label for="">Item Type </label>
+          <select class="custom-select" id="" name="item_type" required>
+            <option selected>Open this select menu</option>
+            <option value="tops">Tops</option>
+            <option value="shoes">Shoes</option>
+            <option value="bags">Bags</option>
+            <option value="accessories">Accessories</option>
+          </select>
+        </div>
+
+        <div class="">
+          <label for="">Item Category </label>
+          <select class="custom-select" id="" name="item_category" required>
+            <option selected>Open this select menu</option>
+            <option value="men">Men</option>
+            <option value="women">Women</option>
+            <option value="kids">Kids</option>
+          </select>
+        </div>
+
+        <div class="">
+          <label for="">Item Description </label>
+          <input class="form-control" type="text" id="" name="item_description" required>
+        </div>
+
+        <div class="">
+          <label for="">Image Uploads </label>
+          <br>
+          <input class="" type="file" id="" name="myfile" required>
+        </div>
+        <br>
+
+        <div class="">
+          <input class="btn btn-outline-secondary" type="submit" name="submit" value="Add Item">
+        </div>
+      </form>
+
+      <br>
+      <br>
+      <p>Index Page &lt; Page for removing new products. &gt;</p>
+      <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-4">
+          <div class="card mb-4 shadow-sm">
+            <!-- <img class="bd-placeholder-img card-img-top" src="../../AfricanPrintSite/africanfashion/tops/top%20(9).jpg" alt="" width="100%" height="225"> -->
+            <img src="holder.js/335x250">
+            <div class="card-body">
+              <p class="card-text">Item Name</p>
+              <p class="card-text">Item description</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <form class="" action="" method="post">
+                  <button type="button" class="openBtn btn btn-sm btn-outline-secondary">Delete Item</button>
+                </form>
+                <small class="text-muted">Ghc 76.74</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6 col-lg-4">
+          <div class="card mb-4 shadow-sm">
+            <!-- <img class="bd-placeholder-img card-img-top" src="../../AfricanPrintSite/africanfashion/tops/top%20(9).jpg" alt="" width="100%" height="225"> -->
+            <img src="holder.js/335x250">
+            <div class="card-body">
+              <p class="card-text">Item Name</p>
+              <p class="card-text">Item description</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <form class="" action="" method="post">
+                  <button type="button" class="openBtn btn btn-sm btn-outline-secondary">Delete Item</button>
+                </form>
+                <small class="text-muted">Ghc 76.74</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6 col-lg-4">
+          <div class="card mb-4 shadow-sm">
+            <!-- <img class="bd-placeholder-img card-img-top" src="../../AfricanPrintSite/africanfashion/tops/top%20(9).jpg" alt="" width="100%" height="225"> -->
+            <img src="holder.js/335x250">
+            <div class="card-body">
+              <p class="card-text">Item Name</p>
+              <p class="card-text">Item description</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <form class="" action="" method="post">
+                  <button type="button" class="openBtn btn btn-sm btn-outline-secondary">Delete Item</button>
+                </form>
+                <small class="text-muted">Ghc 76.74</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
     <!-- JavaScript Frameworks and libraries. -->
+    <script src="../../js/framework/holder.js" charset="utf-8"></script>
     <?php include '../../php/custom/included_pages/common_js.php'; ?>
     <script src="../../js/custom/modal.js" charset="utf-8"></script>
   </body>
---
+++ php/custom/upload_items.php
@@ -0,0 +1,90 @@
+<?php
+// Getting information, from item
+  $name = htmlspecialchars($_POST['item_name']);
+  $description = htmlspecialchars($_POST['item_description']);
+  $item_type = htmlspecialchars($_POST['item_type']);
+  $item_price = htmlspecialchars($_POST['item_price']);
+  $item_category = htmlspecialchars($_POST['item_category']);
+
+
+  // $currentDir = getcwd();
+  $currentDir = __dir__;
+  $uploadDirectory = "\..\..\africanfashion\\";
+  $imgPath = "..\\africanfashion\\";
+
+  if ($item_type == "tops") {
+    $uploadDirectory .= "tops\\";
+    $imgPath .= "tops\\";
+  } elseif ($item_type == "shoes") {
+    $uploadDirectory .= "africanshoes\\";
+    $imgPath .= "africanshoes\\";
+  } elseif ($item_type == "bags") {
+    $uploadDirectory .= "bags\\";
+    $imgPath .= "bags\\";
+  } elseif ($item_type == "accessories") {
+    $uploadDirectory .= "accessories\\";
+    $imgPath .= "accessories\\";
+  } else {
+    // code...
+    $msg = "Invalid Product Type";
+    header("../../html/admin/index.php?msg=".$msg);
+  }
+
+
+  $errors = []; // Store all foreseen and unforseen errors here
+
+  $fileExtensions = ['jpeg','jpg','png']; // Get all the file extensions
+
+  $fileName = $_FILES['myfile']['name'];
+  $fileSize = $_FILES['myfile']['size'];
+  $fileTmpName  = $_FILES['myfile']['tmp_name'];
+  $fileType = $_FILES['myfile']['type'];
+
+  $fileExtension = explode('.',$fileName);
+  $fileExtension = strtolower(end($fileExtension));
+
+  $uploadPath = $currentDir . $uploadDirectory . basename($fileName);
+  $imgPath .= basename($fileName);
+
+ if (isset($_POST['submit'])) {
+
+     if (! in_array($fileExtension,$fileExtensions)) {
+         $errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
+     }
+
+     if ($fileSize > 2000000) {
+         $errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 2MB";
+     }
+
+     if (empty($errors)) {
+         $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+         $msg = "";
+         if ($didUpload) {
+             echo "The file " . basename($fileName) . " has been uploaded";
+         } else {
+             echo "An error occurred somewhere. Try again";
+         }
+     } else {
+         foreach ($errors as $error) {
+             echo $error . "These are the errors" . "\n";
+         }
+     }
+ }
+
+
+
+
+  // Including connection script.
+  include 'included_pages/db_connection.php';
+
+  $sql = "INSERT INTO brand(brand_id, brand_name, brand_description, brand_image, brand_type, brand_price, brand_category)
+	        VALUES (nextval('item_brand_id'), :name, :description, :imageurl, :item_type, :item_price, :item_category);";
+
+  $stmt = $pdo->prepare($sql);
+
+  $stmt->execute(array(':name' => $name, ':description' => $description,
+                       ':imageurl' => $imgPath, ':item_type' => $item_type,
+                       ':item_price' => $item_price, ':item_category' => $item_category))
+
+?>
