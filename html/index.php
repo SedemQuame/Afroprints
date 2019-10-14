3e01fae3471a310e93fd0aa4db9a4f8295738cbe
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-10-14 15:31:30 +0100

    minor change to the index.php page.

--- html/index.php
+++ html/index.php
@@ -36,34 +36,25 @@
           <img class="design_images" src="../africanfashion/africanbags/bag (1).jpg" alt="">
         </div>
         <div class="col-12 col-md-6 infograph">
-          <h1 class="home-page-section-header"> Sample Heading.</h1>
+          <h1 class="home-page-section-header"> Get beautiful hand crafted Ghanaian Bags.</h1>
           <p class="text-center">
-            Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry. Lorem Ipsum has been the industry's
-            standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries,
-            but also the leap into electronic typesetting, remaining
-            essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem
-            Ipsum passages, and more recently with desktop.
+            Find the most interesting patterns in bags, to
+            compliment your outfit for any occasion.
+            Be the first in your circle to rock, the latest
+            African handbags and accessories, rep the
+            culture anywhere.
           </p>
         </div>
       </div>
 
       <div class="row">
         <div class="col-12 order-6 order-md-1 col-md-6 infograph">
-          <h1 class="home-page-section-header"> Sample Heading.</h1>
+          <h1 class="home-page-section-header"> African Inspired Tees.</h1>
           <p class="text-center">
-            Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry. Lorem Ipsum has been the industry's
-            standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries,
-            but also the leap into electronic typesetting, remaining
-            essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem
-            Ipsum passages, and more recently with desktop.
+            Get the latest african tees on the market delivered right onto your door step.
+            Add a color to your wordrobe, rock dresses with witty quotes,mild profanity,
+            and some of out best jokes.
+            Take the african culture with you, everywhere you go.
           </p>
         </div>
         <div class="col-12 order-1 order-md-6 col-md-6 infograph">
@@ -76,17 +67,11 @@
           <img class="design_images" src="../africanfashion/tops/top (2).jpg" alt="">
         </div>
         <div class="col-12 col-md-6 infograph">
-          <h1 class="home-page-section-header"> Sample Heading.</h1>
+          <h1 class="home-page-section-header"> Shoes and accessories.</h1>
           <p class="text-center">
-            Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry. Lorem Ipsum has been the industry's
-            standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries,
-            but also the leap into electronic typesetting, remaining
-            essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem
-            Ipsum passages, and more recently with desktop.
+             Get the latest shoes and accessories, and help
+             support the amazing designers of Africa
+             who possess limitless potential, and creativity.
           </p>
         </div>
       </div>
